099ef285614d27f960b70d0607627f9d1aa3c59f
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2023-12-16 14:44:05 +0530

    final

--- basic_run.sh
+++ basic_run.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
-chmod 777 basic_run.sh
 docker build -t basic-img:latest .
 docker compose up
-~                                                                                                                                                                                                            
-~                                                                                                                                                                                                            
-~                                                                                                                                                                                                            
-~                                                                                                                                                                                                            
-~                                               
